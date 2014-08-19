4fd4d0708bb4e131d859697a23940c67988186ed
mgalgin <mgalgin@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-19 11:20:57 +0000

    test

---
+++ projekty/spring/10/Spring-lab1_00/src/main/java/altkom/Lab0.java
@@ -0,0 +1,27 @@
+package altkom;
+
+import altkom.model.Address;
+import altkom.model.Person;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+
+public class Lab0 {
+	public static void main(String args[]) {
+
+		//Utworz kontener
+            
+            BeanFactory context = new XmlBeanFactory(new ClassPathResource("context.xml"));
+
+		//Pobierz beany z kontenera
+            
+            Person person1 = context.getBean("person", Person.class);
+            Address address1 = context.getBean("address", Address.class);
+            
+		//Wyswietl wlsciwosci beanow
+		System.out.println(person1);
+                System.out.println(address1);
+	} 
+	
+}
