e831ae81e0d3e9cbd364568575bf7b6eff1d45f9
znitecki <znitecki@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-20 13:37:24 +0000

    Commit Nitecki 00

--- projekty/spring/05/Spring-lab1_03/src/main/java/altkom/model/repository/PhoneBookRepository.java
+++ projekty/spring/05/Spring-lab1_03/src/main/java/altkom/model/repository/PhoneBookRepository.java
@@ -8,19 +8,19 @@ import java.util.Map;
 
 public interface PhoneBookRepository {
 
-    public abstract void save(Entry entry);
+    abstract void save(Entry entry);
 
-    public abstract void update(Entry entry);
+    abstract void update(Entry entry);
 
-    public abstract Entry findById(Long id);
+    abstract Entry findById(Long id);
 
-    public abstract List<Entry> findAllEntries();
+    abstract List<Entry> findAllEntries();
 
-    public abstract void logToSysout();
+    abstract void logToSysout();
 
     abstract void setCars(Map<Long, Car> cars);
 
     abstract void setCarsAsList(List<Car> cars);
 
-    public abstract List<Car> findAllCars();
+    abstract List<Car> findAllCars();
 }
