9d7dae0bf912e44691fcd6c3c90a7db88f4556d2
znitecki <znitecki@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-20 09:58:32 +0000

    Commit Nitecki 00

---
+++ projekty/spring/05/JavaApplication2/src/javaapplication2/Car.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author kursant5
+ */
+public class Car {
+
+    private String marka;
+
+    public Car(String marka) {
+        this.marka = marka;
+    }
+
+    public Car() {
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+}
---
+++ projekty/spring/05/JavaApplication2/src/javaapplication2/TestApp.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author kursant5 8=====D
+ */
+public class TestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Car car1 = new Car("Polonez");
+        Car car2 = new Car("Maluch");
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+
+        for (Car car : cars) {
+            System.out.println(car.getMarka());
+        }
+
+    }
+
+}
