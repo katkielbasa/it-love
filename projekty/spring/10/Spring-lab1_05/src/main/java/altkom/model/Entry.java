89266400214f578a767ad6c7a4d5f7cfd0052c3e
mgalgin <mgalgin@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-21 09:56:59 +0000

    test

--- projekty/spring/10/Spring-lab1_05/src/main/java/altkom/model/Entry.java
+++ projekty/spring/10/Spring-lab1_05/src/main/java/altkom/model/Entry.java
@@ -3,71 +3,77 @@ package altkom.model;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class Entry {
-	
-	private Long id;
-	
-	private EntryCategory category = EntryCategory.DEFAULT;
-	
-	private Person person = new Person();
-	
-	private Address address = new Address();
-	
-	private Phone phone = new Phone();
-	
-	@Override
-	public String toString() {
-		
-		return ToStringBuilder.reflectionToString( this );
-	}
-	
-	public Long getId() {
-	
-		return id;
-	}
-	
-	public void setId( Long id ) {
-	
-		this.id = id;
-	}
 
-	public EntryCategory getCategory() {
-	
-		return category;
-	}
+    private Long id;
 
-	public Person getPerson() {
-	
-		return person;
-	}
+    private EntryCategory category = EntryCategory.DEFAULT;
 
-	public Address getAddress() {
-	
-		return address;
-	}
+    private Person person = new Person();
 
-	public void setCategory( EntryCategory category ) {
-	
-		this.category = category;
-	}
-	
-	public void setPerson( Person person ) {
-	
-		this.person = person;
-	}
-	
-	public void setAddress( Address address ) {
-	
-		this.address = address;
-	}
+    private Address address = new Address();
 
-	public Phone getPhone() {
+    private Phone phone = new Phone();
 
-		return phone;
-	}
+    private Car car = new Car();
 
-	public void setPhone(Phone phone) {
+    @Override
+    public String toString() {
 
-		this.phone = phone;
-	}
-	
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    public EntryCategory getCategory() {
+
+        return category;
+    }
+
+    public Person getPerson() {
+
+        return person;
+    }
+
+    public Address getAddress() {
+
+        return address;
+    }
+
+    public void setCategory(EntryCategory category) {
+
+        this.category = category;
+    }
+
+    public void setPerson(Person person) {
+
+        this.person = person;
+    }
+
+    public void setAddress(Address address) {
+
+        this.address = address;
+    }
+
+    public Phone getPhone() {
+
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+
+        this.phone = phone;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
 }
