e4195b7aa51ba3cb4d3756178b63a76426f9ec03
znitecki <znitecki@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-21 12:26:42 +0000

    Commit Nitecki 00

---
+++ projekty/spring/05/Spring-lab1_09/src/main/java/altkom/controller/AddEntryController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altkom.controller;
+
+import altkom.model.Category;
+import altkom.model.Entry;
+import altkom.model.repository.PhoneBookRepository;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author kursant5
+ */
+@Controller
+@RequestMapping("/addEntry.do")
+public class AddEntryController {
+
+    private final static String FORM_VIEW = "addEntryForm";
+
+    private final static String SUCCESS_VIEW = "home";
+
+    public List prepareCategories() {
+        List categories = new ArrayList();
+        categories.add(Category.DEFAULT);
+        categories.add(Category.CUSTOMER);
+        categories.add(Category.BUSINESS_PARTNER);
+        categories.add(Category.SERVICE_DELIVERER);
+
+        return categories;
+    }
+    @Resource(name = "inMemoryRepository")
+    private PhoneBookRepository phoneBookRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String setupForm(Model model) {
+        model.addAttribute(new Entry());
+        model.addAttribute("categories", prepareCategories());
+
+        return FORM_VIEW;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute Entry entry) {
+        System.out.println("Category = " + entry.getCategory());
+        phoneBookRepository.save(entry);
+        phoneBookRepository.logToSysout();
+
+        return SUCCESS_VIEW;
+    }
+}
--- projekty/spring/05/Spring-lab1_09/src/main/webapp/WEB-INF/jsp/addEntryForm.jsp
+++ projekty/spring/05/Spring-lab1_09/src/main/webapp/WEB-INF/jsp/addEntryForm.jsp
@@ -1,26 +1,42 @@
 <%@ include file="./includes/common.jsp" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
-<body>
-	<h1>Dodaj wpis do książki adresowej:</h1>
-	<form:form modelAttribute="entry">
-		<table>
-		<tr>
-			<td>
-				Kategoria:
-			</td>
-			<td>
-				<form:select path="category" items="${categories}"/>
-				
-			</td>
-		</tr>
-		
-		<tr>
-			<td>
-				<input type="submit" value="Zapisz" colspan="2" />
-			</td>
-		</tr>
-		</table>
-	</form:form>
-</body>
+    <body>
+        <h1>Dodaj wpis do książki adresowej:</h1>
+        <form:form modelAttribute="entry">
+            <table>
+                <tr>
+                    <td>
+                        Kategoria:
+                    </td>
+                    <td>
+                        <form:select path="category" items="${categories}"/>
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <input type="submit" value="Zapisz" colspan="2" />
+                    </td>
+                </tr>
+            </table>
+            <table border="1">
+                <tr>
+                    <td>Imię:</td>
+                    <td>
+                        <form:input path="person.name" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Nazwisko:</td>
+                    <td>
+                        <form:input path="person.surname" />
+                    </td>
+                </tr>
+            </table>
+            <hr/>
+        </form:form>
+    </body>
 </html>
