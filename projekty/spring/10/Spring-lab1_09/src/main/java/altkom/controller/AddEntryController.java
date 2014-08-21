7a8d4f357cdd4e1c18ab1c5c1d6c9beff91e4015
mgalgin <mgalgin@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-21 14:53:11 +0000

    test

---
+++ projekty/spring/10/Spring-lab1_09/src/main/java/altkom/controller/AddEntryController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altkom.controller;
+
+import altkom.model.Category;
+import altkom.model.Entry;
+import altkom.model.repository.PhoneBookRepository;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/addEntry.do")
+public class AddEntryController {
+
+    private final static String FORM_VIEW = "addEntryForm";
+
+    private final static String SUCCESS_VIEW = "home";
+
+    @Resource(name = "inMemoryRepository")
+    private PhoneBookRepository phoneBookRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String setupForm(Model model) {
+        model.addAttribute(new Entry());
+        model.addAttribute("categories", prepareCategories());
+
+        return FORM_VIEW;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute Entry entry) {
+        System.out.println( "Category = " + entry.getCategory() );
+		phoneBookRepository.save( entry );
+		phoneBookRepository.logToSysout();
+
+
+                //---zapis do bazy i zalogowanie
+        return SUCCESS_VIEW;
+    }
+    public List prepareCategories() {
+		List categories = new ArrayList();
+		categories.add( Category.DEFAULT );
+		categories.add( Category.CUSTOMER );
+		categories.add( Category.BUSINESS_PARTNER );
+		categories.add( Category.SERVICE_DELIVERER );
+		
+		return categories;
+	}
+}
