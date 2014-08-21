7c12a8e5e94d64bc9ee719c5b24a67e849e27e35
mgalgin <mgalgin@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-21 08:11:43 +0000

    test

---
+++ projekty/spring/10/Spring-lab1_05/src/main/java/altkom/model/editors/PersonEditor.java
@@ -0,0 +1,15 @@
+package altkom.model.editors;
+
+import java.beans.PropertyEditorSupport;
+import java.util.StringTokenizer;
+
+import altkom.model.Person;
+
+public class PersonEditor extends PropertyEditorSupport {
+
+	@Override
+	public void setAsText( String text ) throws IllegalArgumentException {
+
+	}
+	
+}
