dcd85438848746138e4e3a10c56d6f2a3e329db2
kkolesnik <kkolesnik@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-28 15:10:10 +0000

    28.08 4th

--- projekty/spring/01/JavaScript/public_html/modul1/cw1.js
+++
@@ -1,6 +0,0 @@
-var text = '';
-
-for (var idx = 1; idx < 7; idx++) {
-    text += idx;
-    console.log(text);
-}
--- projekty/spring/01/JavaScript/public_html/modul3/SelfInvokingFunction.js
+++
@@ -1,6 +0,0 @@
-var a = (function test(name) {
-    console.log('hello ' + name);
-    return 1;
-})('jan');
-
-console.log(a);
