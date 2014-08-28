299a12d3b9162c7db798e96828c8137384f9a6bb
kkolesnik <kkolesnik@11a6dc57-d676-4505-b7dc-48ad575a0bd8>
2014-08-28 15:10:23 +0000

    28.08 4th

---
+++ projekty/spring/01/JavaScriptInstruktora/public_html/modul1/cw6.js
@@ -0,0 +1,16 @@
+var sum = add(3,4, 5, 6,345,345,345,34534);
+console.log(sum);
+
+    b = 6;
+    c = 4;
+
+function add(a, b) {    
+    
+    return a + b;
+}
+
+function test() {
+    console.log(b);
+}
+
+ console.log(b);
